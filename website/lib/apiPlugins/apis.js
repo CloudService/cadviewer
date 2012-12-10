0674d0dc62c64fb270e0336ca4ecb519385c00f0
Jeffrey <Jeffrey.zksun@gmail.com>
2012-12-10 21:43:32 +0800

    Finish the API GET /api/1.0/files/:id

--- website/lib/apiPlugins/apis.js
+++ website/lib/apiPlugins/apis.js
@@ -2,6 +2,9 @@
 * This is the tempalte of the route module. Copy this file when add new api route module.
 */
 
+var everyauth = require('everyauth');
+var request = require('request');
+
 /**********************************************************************/
 // Define and implement the REST api.
 /**********************************************************************/
@@ -31,9 +34,10 @@ var addRoute = function(options){
 	var nextTaskId=0;
 	var build = config.get('build');
 	var server = config.get(build);
+	var importFormats = config.get('formats')['import'];
 	
 	expressApp.post('/api/1.0/tasks', function(req, res, next){
-		logger.info("==> /api/1.0/tasks");
+		logger.debug("==> /api/1.0/tasks");
 		// req.body saves posted JSON object.
 		var task = req.body;
 		var taskString = JSON.stringify(task);
@@ -54,33 +58,147 @@ var addRoute = function(options){
 	});
 	
 	expressApp.get('/api/1.0/files/:id', function(req, res, next){
-
-		apiErrorManager.responseNotImplemented(res);
+		logger.debug("==> /api/1.0/files/:id");
 		
+		if(!req.session){
+			apiErrorManager.responseUnauthorized(res);
+		}
+		
+		if(!req.session.auth){
+			apiErrorManager.responseUnauthorized(res);
+		}
+		
+		if(!req.session){
+			apiErrorManager.responseUnauthorized(res);
+		}
+		
+		if(!req.session.auth.box){
+			apiErrorManager.responseUnauthorized(res);
+		}
+		
+		if(!req.session.auth.box.authToken){
+			apiErrorManager.responseUnauthorized(res);
+		}
+		
+		var id = req.params.id;
+		// Get root folder
+		var url = 'https://www.box.com/api/2.0/folders/' + id;
+		var apiKey = server.box.apiKey;
+		var access_token = req.session.auth.box.authToken;//"gymnnxkbxikj0jm6rz25cq4kwe8go808";
+		var headers = {Authorization: "BoxAuth api_key=" + apiKey + "&auth_token=" + access_token};
+	
+		/*
+		The format of the return value is like.
+		"{
+			"type": "folder",
+			"id": "0",
+			"sequence_id": null,
+			"name": "AllFiles",
+			"created_at": null,
+			"modified_at": null,
+			"description": null,
+			"size": 0,
+			"created_by": {
+				"type": "user",
+				"id": "185684932",
+				"name": "transMr",
+				"login": "3dcadtrans@gmail.com"
+			},
+			"modified_by": {
+				"type": "user",
+				"id": "185684932",
+				"name": "transMr",
+				"login": "3dcadtrans@gmail.com"
+			},
+			"owned_by": {
+				"type": "user",
+				"id": "185684932",
+				"name": "transMr",
+				"login": "3dcadtrans@gmail.com"
+			},
+			"shared_link": null,
+			"parent": null,
+			"item_status": "active",
+			"item_collection": {
+				"total_count": 2,
+				"entries": [
+					{
+						"type": "folder",
+						"id": "419345934",
+						"sequence_id": "0",
+						"name": "Web"
+					},
+					{
+						"type": "folder",
+						"id": "419345748",
+						"sequence_id": "0",
+						"name": "Robot"
+					}
+				]
+			}
+		}"
+		*/
+		request.get({url:url, headers:headers}, function (e, r, body) {	
+			var boxFolderInfo = JSON.parse(body);
+			var boxFileList = boxFolderInfo.item_collection.entries;
+			
+			var fileObject = {
+				"type": "file",
+				"id": id,
+				"name": boxFolderInfo.name,
+				"is_folder": boxFolderInfo.type === "folder",
+				"size": boxFolderInfo.size,
+				"modified_at": "2012-07-27T09:22:05Z",
+				"children": []
+			}
+
+			
+			var subFiles = [];
+			var length = boxFileList.length;
+			for(var i = 0; i < length; ++i){
+				var entry = boxFileList[i];
+				var isFolder = entry.type === "folder";
+				var name = entry.name;
+				if(!isFolder){
+					// Check it the file is the supported format.
+					var ext = name.split('.').pop();
+					 var isSupported = (importFormats.indexOf(ext) != -1);
+					 if(!isSupported)
+					 	continue;
+				}
+				var file = {};
+				file.type="file";
+				file.id = entry.id;
+				file.name=name;
+				file.is_folder = isFolder;
+				file.size = "-"; // Todo get the correct value;
+				file.modified_at = "2012-10-4 9:45"; // Todo get the correct value;
+				subFiles.push(file);
+			}
+			
+			fileObject.children = subFiles;
+			
+			res.send(200, fileObject);
+		});  
 	});
 	
 	expressApp.get('/api/1.0/tasks', function(req, res, next){
-
+		
 		var task = taskManager.pendingTranslationTasks.splice(0,1); // Pop the front one.
 		
 		res.send(task);
 		
 	});
 	
-	expressApp.post('/api/1.0/jobs/import', function(req, res, next){
-
-		apiErrorManager.responseNotImplemented(res);
-		
-	});
-	
 	expressApp.get('/api/1.0/models/:id', function(req, res, next){
-
+		
 		apiErrorManager.responseNotImplemented(res);
 		
 	});
 	
 	expressApp.put('/api/1.0/models/:id', function(req, res, next){
-
+		logger.debug("==> /api/1.0/models/:id");
+		
 		apiErrorManager.responseNotImplemented(res);
 		
 	});
