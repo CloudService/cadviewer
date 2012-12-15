a06f3dcd59538c86b6f2f4c8bce8e8e884658c30
Jeffrey <Jeffrey.zksun@gmail.com>
2012-12-15 18:14:09 +0800

    Display the file import dialog after the authorization.

--- website/server.js
+++ website/server.js
@@ -86,13 +86,7 @@ expressApp.get('/', function(req, res, next){
 	var boxAuth = auth ? auth.box : null;
 	var showDefault = req.query.d ? true : false; // http://server.com/?d=1
 	
-	var workerNum = 0;
-	var taskNum = 0;
-	var locals = { "amessage": ""
-					  , "workerNum": workerNum
-					  , "taskNum" : taskNum 
-					  , "destFormats": JSON.stringify(exportFormats)};
-					  
+	var locals = { "amessage": ""};				  
 	
 	if(showDefault || !boxAuth)
 		renderDefaultPage(req, res, next, locals);
@@ -105,103 +99,17 @@ expressApp.get('/', function(req, res, next){
    	}
    	 
    	function renderWithFileDialog(req, res, next, locals){
-   	
-		// Get root folder
-		var url = 'https://www.box.com/api/2.0/folders/0';
-		var apiKey = server.box.apiKey;
-		var access_token = req.session.auth.box.authToken;//"gymnnxkbxikj0jm6rz25cq4kwe8go808";
-		var headers = {Authorization: "BoxAuth api_key=" + apiKey + "&auth_token=" + access_token};
-	
-		/*
-		The format of the return value is like.
-		"{
-			"type": "folder",
-			"id": "0",
-			"sequence_id": null,
-			"name": "AllFiles",
-			"created_at": null,
-			"modified_at": null,
-			"description": null,
-			"size": 0,
-			"created_by": {
-				"type": "user",
-				"id": "185684932",
-				"name": "transMr",
-				"login": "3dcadtrans@gmail.com"
-			},
-			"modified_by": {
-				"type": "user",
-				"id": "185684932",
-				"name": "transMr",
-				"login": "3dcadtrans@gmail.com"
-			},
-			"owned_by": {
-				"type": "user",
-				"id": "185684932",
-				"name": "transMr",
-				"login": "3dcadtrans@gmail.com"
-			},
-			"shared_link": null,
-			"parent": null,
-			"item_status": "active",
-			"item_collection": {
-				"total_count": 2,
-				"entries": [
-					{
-						"type": "folder",
-						"id": "419345934",
-						"sequence_id": "0",
-						"name": "Web"
-					},
-					{
-						"type": "folder",
-						"id": "419345748",
-						"sequence_id": "0",
-						"name": "Robot"
-					}
-				]
-			}
-		}"
-		*/
-		request.get({url:url, headers:headers}, function (e, r, body) {	
-			var boxFolderInfo = JSON.parse(body);
-			var boxFileList = boxFolderInfo.item_collection.entries;
-			
-			var subFiles = [];
-			var length = boxFileList.length;
-			for(var i = 0; i < length; ++i){
-				var entry = boxFileList[i];
-				var isFolder = entry.type === "folder";
-				var name = entry.name;
-				if(!isFolder){
-					// Check it the file is the supported format.
-					var ext = name.split('.').pop();
-					 var isSupported = (importFormats.indexOf(ext) != -1);
-					 if(!isSupported)
-					 	continue;
-				}
-				var file = {};
-				file.name=name;
-				file.isFolder = isFolder;
-				file.id = entry.id;
-				file.size = "-"; // Todo get the correct value;
-				file.moddate = "2012-10-4 9:45"; // Todo get the correct value;
-				subFiles.push(file);
-			}
-			
-			var aobject = {
-				handler: "translator"
-				, action: "openFileDialog"
-				, data: {files: subFiles}
-				};
-						
-			var amessage = JSON.stringify(aobject);
-			logger.info(amessage);
-			
-			locals['amessage'] = amessage;
+ 		var aobject = {
+			handler: "translator"
+			, action: "openFileDialog"
+		};
 					
-		   	res.render('index', {'locals': locals});
-		});  
+		var amessage = JSON.stringify(aobject);
+		logger.info(amessage);
+		
+		locals['amessage'] = amessage;
+				
+		res.render('index', {'locals': locals});
    	}  
 });
 
