b10a5e2525f7312e3dae5856b6e7c058b4f890fb
Jeffrey <Jeffrey.zksun@gmail.com>
2012-12-30 15:36:05 +0800

    Update the API documents.
    Use YUIDoc to generate the API documents.

---
+++ website/public/dev/docs/api.js
@@ -0,0 +1,24 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "apis",
+        "samples"
+    ],
+    "modules": [
+        "apis",
+        "samples"
+    ],
+    "allModules": [
+        {
+            "displayName": "apis",
+            "name": "apis",
+            "description": "This is api module."
+        },
+        {
+            "displayName": "samples",
+            "name": "samples",
+            "description": "Implement the APIs for the sample object."
+        }
+    ]
+} };
+});
